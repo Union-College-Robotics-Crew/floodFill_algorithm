934f2957c96b1038a980186b24dc889f8ec4e444
manavbilakhia <57500134+manavbilakhia@users.noreply.github.com>
2022-01-31 18:50:16 -0500

    Update micromouse_interface.py

--- micromouse_interface.py
+++ micromouse_interface.py
@@ -1,2 +1,56 @@
 
 class Micromouse():
+  def command(args, return_type=None):
+    pass
+  def mazeWidth():
+    pass
+  def mazeHeight():
+    pass
+
+  def wallFront():
+    pass
+
+
+  def wallRight():
+    pass
+  def wallLeft():
+    pass
+
+  def moveForward(distance=None):
+    pass
+
+  def turnRight():
+    pass
+
+  def turnLeft():
+    pass
+
+  def setWall(x, y, direction):
+    pass
+
+  def clearWall(x, y, direction):
+    pass
+
+  def setColor(x, y, color):
+    pass
+
+  def clearColor(x, y):
+    pass
+
+  def clearAllColor():
+    pass
+
+  def setText(x, y, text):
+    pass  
+
+  def clearText(x, y):
+    pass
+
+  def clearAllText():
+    pass
+
+  def wasReset():
+    pass
+
+  def ackReset():
+    pass
